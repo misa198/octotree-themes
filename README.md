04db474f28a17b2d3f4d7d353c4d87a47b6461c4
Thanh Vu <csnsjh@gmail.com>
2021-10-02 16:26:56 +0700

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# GitHub File Icons
+
+<img width="155" alt="screen shot 2018-08-07 at 11 52 14" src="https://user-images.githubusercontent.com/12113222/43749677-67bc7a86-9a38-11e8-9ed2-f5f762caacd4.png">
+
+<p>
+<a target="_blank" href="https://opensource.org/licenses/MIT" title="License: MIT"><img src="https://img.shields.io/badge/License-MIT-blue.svg"></a>
+<a target="_blank" href="http://makeapullrequest.com" title="PRs Welcome"><img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg"></a>
+</p>
+
+> Inspired by [file-icons/atom](https://github.com/file-icons/atom)
+>
+> A simple browser tool changes file's icon on GitHub, GitLab, gitea and gogs.
+
+## Install
+
+<a href="https://chrome.google.com/webstore/detail/github-file-icons/ficfmibkjjnpogdcfhfokmihanoldbfe">
+  <img border="0" src="https://storage.googleapis.com/chrome-gcs-uploader.appspot.com/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/HRs9MPufa1J1h5glNhut.png" width="172" >
+</a>
+<br/>
+<a href="https://addons.mozilla.org/en-US/firefox/addon/github-file-icons/">
+<img border="0" src="https://addons.cdn.mozilla.net/static/img/addons-buttons/AMO-button_1.png" width="172">
+</a>
+
+## Options
+
+> Right click on any space on the page or the extension's icon to open the menu.
+
+![untitled](https://user-images.githubusercontent.com/12113222/51907474-02ba6e00-241b-11e9-8199-83a18502fb6b.png)
+
+### Change image colors
+
+> Change icon's color between colorful and monochrome.
+
+##### Comparison:
+
+<img width="157" alt="screen shot 2018-08-07 at 11 56 51" src="https://user-images.githubusercontent.com/12113222/43754021-f9b40946-9a4b-11e8-8144-ab7cb5cbea8e.png">
+
+### Toggle Dark Mode
+
+> Make icons brighter and more readable on [GitHub dark theme](https://github.com/StylishThemes/GitHub-Dark).
+
+##### Comparison:
+
+![brightness comparison](https://user-images.githubusercontent.com/454813/38455054-4a924cf4-3a40-11e8-86d6-29a030dfde49.jpg)
+
+### Enable on custom domain
+
+> Navigate to your self-hosted Git site. Right click on the extension's icon in the browser bar and click "Enable File Icon for GitHub and GitLab on this domain".
+
+![Screen Shot 2019-08-08 at 21 01 19](https://user-images.githubusercontent.com/12113222/62698132-b7af9200-ba1f-11e9-9edc-396b6b9295be.png)
+
+## License
+
+MIT © [homerchen19](https://github.com/homerchen19)
---
+++ src/ts/content.ts
@@ -0,0 +1,161 @@
+import * as fileIcons from 'file-icons-js';
+import * as domLoaded from 'dom-loaded';
+import select from 'select-dom';
+import mobile from 'is-mobile';
+import { observe } from 'selector-observer';
+
+import { StorageKey } from './background';
+import '../css/icons.css';
+
+let colorsDisabled = false;
+let darkMode = false;
+
+const fonts = [
+  { name: 'FontAwesome', path: 'fonts/fontawesome.woff2' },
+  { name: 'Mfizz', path: 'fonts/mfixx.woff2' },
+  { name: 'Devicons', path: 'fonts/devopicons.woff2' },
+  { name: 'file-icons', path: 'fonts/file-icons.woff2' },
+  { name: 'octicons', path: 'fonts/octicons.woff2' },
+];
+
+const loadFonts = () => {
+  fonts.forEach((font) => {
+    const fontFace = new FontFace(
+      font.name,
+      `url("${chrome.extension.getURL(font.path)}") format("woff2")`,
+      {
+        style: 'normal',
+        weight: 'normal',
+      }
+    );
+
+    fontFace
+      .load()
+      .then((loadedFontFace) => document.fonts.add(loadedFontFace));
+  });
+};
+
+const getGitHubMobileFilename = (filenameDom: HTMLElement) =>
+  Array.from(filenameDom.childNodes)
+    .filter((node) => node.nodeType === node.TEXT_NODE)
+    .map((node) => node.nodeValue!.trim())
+    .join('');
+
+const isMobile = mobile();
+
+const replaceIcon = ({
+  iconDom,
+  filenameDom,
+}: {
+  iconDom: HTMLElement | null;
+  filenameDom: HTMLElement;
+}) => {
+  const filename = isMobile
+    ? getGitHubMobileFilename(filenameDom)
+    : filenameDom.innerText.trim();
+
+  let isDirectory = false;
+  if (iconDom) {
+    isDirectory = iconDom.classList.contains('octicon-file-directory');
+  }
+
+  const className: string | null = colorsDisabled
+    ? fileIcons.getClass(filename)
+    : fileIcons.getClassWithColor(filename);
+
+  const darkClassName = darkMode ? 'dark' : '';
+
+  if (className && !isDirectory) {
+    const icon = document.createElement('span');
+    icon.className = `icon octicon-file ${className} ${darkClassName}`;
+    if (iconDom) {
+      iconDom.parentNode!.replaceChild(icon, iconDom as HTMLElement);
+    }
+  }
+};
+
+const replaceOctotreeIcon = ({
+  iconDom,
+  filenameDom,
+}: {
+  iconDom: HTMLElement | null;
+  filenameDom: HTMLElement;
+}) => {
+  const filename = isMobile
+    ? getGitHubMobileFilename(filenameDom)
+    : filenameDom.innerText.trim();
+
+  let isDirectory = false;
+  if (iconDom) {
+    isDirectory = iconDom.classList.contains('octotree-icon-file-directory');
+    isDirectory = iconDom.classList.contains('jstree-closed');
+    isDirectory = iconDom.classList.contains('jstree-open');
+  }
+
+  const className: string | null = colorsDisabled
+    ? fileIcons.getClass(filename)
+    : fileIcons.getClassWithColor(filename);
+
+  const darkClassName = darkMode ? 'dark' : '';
+
+  if (className && !isDirectory) {
+    const icon = document.createElement('span');
+    icon.className = `icon octicon-file misa198-octotree-icon ${className} ${darkClassName}`;
+
+    if (iconDom) {
+      iconDom.parentNode!.replaceChild(icon, iconDom as HTMLElement);
+    }
+  }
+};
+
+const init = async () => {
+  loadFonts();
+  await domLoaded;
+
+  observe('.js-navigation-container > .js-navigation-item', {
+    add(element) {
+      const filenameDom = select('div[role="rowheader"] > span', element);
+
+      if (!filenameDom) {
+        return;
+      }
+
+      replaceIcon({
+        iconDom: select('.octicon-file', element) as HTMLElement,
+        filenameDom,
+      });
+    },
+  });
+
+  observe('.jstree-node', {
+    add(element) {
+      const filenameDom = select('.jstree-anchor > div', element);
+
+      if (!filenameDom) {
+        return;
+      }
+
+      replaceOctotreeIcon({
+        iconDom: select(
+          '.jstree-anchor > .jstree-icon',
+          element
+        ) as HTMLElement,
+        filenameDom,
+      });
+    },
+  });
+};
+
+chrome.storage.sync.get(
+  [StorageKey.ColorsDisabled, StorageKey.DarkMode],
+  (result) => {
+    colorsDisabled =
+      result.colorsDisabled === undefined
+        ? colorsDisabled
+        : result.colorsDisabled;
+
+    darkMode = result.darkMode === undefined ? darkMode : result.darkMode;
+
+    init();
+  }
+);
